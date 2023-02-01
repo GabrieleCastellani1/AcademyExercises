c242f6ea2004d5e843f7728c36dc8e6b3c27d36b
Gabriele <castellanigabriele1@gmail.com>
2023-02-01 12:54:12 +0100

    update chess class

---
+++ it/euris/academy2023/chess/Piece.java
@@ -0,0 +1,39 @@
+package it.euris.academy2023.chess;
+
+import java.util.Objects;
+
+public abstract class Piece {
+    protected int x;
+    protected int y;
+
+    public final boolean move(int x, int y){
+        boolean b = x >= 0 && x <= 7 && y >= 0 && y <= 7 && validate(x,y);
+        if(b){
+            this.x = x;
+            this.y = y;
+        }
+        return b;
+    }
+
+    public abstract boolean validate(int x, int y);
+    protected Piece(){}
+
+    protected Piece(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o){
+            return true;
+        }else {
+            return this.getClass().getName().equals(o.getClass().getName());
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getClass().getName());
+    }
+}
